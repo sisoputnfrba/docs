95e37082a51e1e7928534be0b10d84974404bec0
Agustin Ranieri <aguseranieri@gmail.com>
2022-10-20 12:39:17 -0300

    agrego code groups

---
+++ docs/.vitepress/components/CodeGroupItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="code-block"
+    :class="{ 'active': active }"
+    :aria-selected="active"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'CodeGroupItem',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+})
+</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,17 @@
 import DefaultTheme from 'vitepress/theme';
+import CodeGroup from '../components/CodeGroup.ts';
+import CodeGroupItem from '../components/CodeGroupItem.vue';
 import YouTube from '../components/YouTube.vue';
+
+import './custom.scss';
+import './code-group.scss';
 
 export default {
   ...DefaultTheme,
   enhanceApp: (ctx) => {
     //DefaultTheme.enhanceApp(ctx);
+    ctx.app.component('CodeGroup', CodeGroup);
+    ctx.app.component('CodeGroupItem', CodeGroupItem);
     ctx.app.component('YouTube', YouTube);
   },
 }
