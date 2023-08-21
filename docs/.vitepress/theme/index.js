b9c63dd3358ce67f4b2dfff7fb5e8d47c7a56ecc
Agustin <aguseranieri@gmail.com>
2023-08-21 16:06:58 -0300

    Generar página de guías desde sidebar

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,6 @@
 import DefaultTheme from 'vitepress/theme';
 import Image from './components/Image.vue';
+import Link from './components/Link.vue';
 import YouTube from './components/YouTube.vue';
 
 import './styles/custom.scss';
@@ -10,6 +11,7 @@ export default {
   enhanceApp: (ctx) => {
     //DefaultTheme.enhanceApp(ctx);
     ctx.app.component('Image', Image);
+    ctx.app.component('Link', Link);
     ctx.app.component('YouTube', YouTube);
   },
 }
