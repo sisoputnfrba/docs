f75c16e7f4f44089d004b63644720cf810955ab1
Agustin Ranieri <aguseranieri@gmail.com>
2024-03-17 23:49:12 -0300

    Agrego posibilidad de hacerle zoom a las imágenes

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,17 +1,32 @@
 import DefaultTheme from 'vitepress/theme';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
 import Image from './components/Image.vue';
 import Link from './components/Link.vue';
 import YouTube from './components/YouTube.vue';
 
-import './styles/custom.scss';
-import './styles/print.scss';
+import './index.scss';
 
 export default {
   ...DefaultTheme,
-  enhanceApp: (ctx) => {
+  enhanceApp(ctx) {
     //DefaultTheme.enhanceApp(ctx);
     ctx.app.component('Image', Image);
     ctx.app.component('Link', Link);
     ctx.app.component('YouTube', YouTube);
+  },
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
   },
 }
